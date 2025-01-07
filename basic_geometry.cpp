acf91fd7e82bf9a5d319591649e575bedc1d7980
JamesRtoP <jrp@JR-Portible>
2025-01-06 18:17:17 -0800

    Creation of basic geometry.hpp and .cpp

---
+++ basic_geometry.cpp
@@ -0,0 +1,139 @@
+#include "basic_geometry.hpp"
+
+point::point()
+{
+    x=0;
+    y=0;
+    z=0;
+}
+point::point(double newX, double newY, double newZ)
+{
+    x = newX;
+	y = newY;
+	z = newZ;
+}
+double point::getx(void) const
+{
+    return x;
+}
+double point::gety(void) const
+{
+	return y;
+}
+double point::getz(void) const
+{
+    return z;
+}
+
+std::ostream& operator<< (std::ostream& stream, const point & aPoint)
+{
+	stream << "(" << aPoint.getx() <<"," << aPoint.gety() << "," << aPoint.getz() << ")";
+	return stream;
+}
+
+
+vector::vector()
+{
+
+    this->xdir = 0;
+    this->ydir = 0;
+    this->zdir = 0;
+}
+vector::vector(double newXDir, double newYDir, double newZDir)
+{
+    this->xdir = newXDir;
+    this->ydir = newYDir;
+    this->zdir = newZDir;
+}
+double vector::getXDir(void) const
+{
+    return this->xdir;
+}
+double vector::getYDir(void) const
+{
+    return this->ydir;
+}
+double vector::getZDir(void) const
+{
+    return this->zdir;
+}
+void vector::setXDir(double newxdir)
+{
+    this->xdir = newxdir;
+}
+void vector::setYDir(double newydir)
+{
+    this->ydir = newydir;
+}
+void vector::setZDir(double newzdir)
+{
+    this->zdir = newzdir;
+}
+vector& vector::operator/(double divisor)
+{
+    this->xdir /= divisor;
+    this->ydir /= divisor;
+    this->zdir /= divisor;
+    return *this;
+}
+vector& vector::operator*(double divisor)
+{
+    this->xdir *= divisor;
+    this->ydir *= divisor;
+    this->zdir *= divisor;
+    return *this;
+}
+
+double dotProduct(vector one, vector two)
+{
+	return one.getXDir() * two.getXDir() +one.getYDir() * two.getYDir() + one.getZDir() * two.getZDir();	
+}
+double getMagnitude(vector v)
+{
+	return sqrt(dotProduct(v,v));
+}
+vector makeUnitVector(vector v)
+{
+	return v / getMagnitude(v);
+}
+point pointFromVector(vector v, double t)
+{
+	return point(v.getXDir()*t, v.getYDir()*t, v.getZDir()*t);
+}
+
+
+void ray::setT(double newT)
+{
+    this->t = newT;
+}
+void ray::setDir(vector newDir)
+{
+    this->dir = newDir;
+}
+void ray::setOrigin(point newOrigin)
+{
+    this->origin = newOrigin;
+}
+double ray::getT(void) const
+{
+    return this->t;
+}
+point ray::getOrigin(void)
+{
+    return this->origin;
+}
+vector ray::getDir(void)
+{	
+    return this->dir;
+}
+
+std::ostream& operator<< (std::ostream& stream, const vector & printMe)
+{
+	stream << "[" << printMe.getXDir() <<"," << printMe.getYDir() << "," << printMe.getZDir() << "]";
+	return stream;
+}
+
+
+
+
+
---
+++ basic_geometry.hpp
@@ -0,0 +1,58 @@
+#include "source.hpp"
+
+class point
+{
+private:
+	double x;
+	double y;
+	double z;
+public:
+	point();
+	point(double newX, double newY, double newZ);
+	double getx(void) const;
+	double gety(void) const;
+	double getz(void) const;
+};
+std::ostream& operator<< (std::ostream& stream, const point & aPoint);
+
+class vector
+{
+private:
+	double xdir;
+	double ydir;
+	double zdir;
+public:
+	vector();
+	vector(double newXDir, double newYDir, double newZDir);
+	double getXDir(void) const;
+	double getYDir(void) const;
+	double getZDir(void) const;
+	void setXDir(double newxdir);
+	void setYDir(double newydir);
+	void setZDir(double newzdir);
+
+	vector& operator/(double divisor);
+	vector& operator*(double divisor);
+};
+
+double dotProduct(vector one, vector two);
+double getMagnitude(vector v);
+vector makeUnitVector(vector v);
+point pointFromVector(vector v, double t);
+
+class ray
+{
+private:
+	vector dir;
+	point origin;
+	//x,y,z for start point
+	double t;//length of vector ie t[x,z,y]
+public:
+	void setT(double newT);
+	void setDir(vector newDir);
+	void setOrigin(point newOrigin);
+	double getT(void) const;
+	point getOrigin(void);
+	vector getDir(void);
+};
+std::ostream& operator<< (std::ostream& stream, const vector & printMe);
